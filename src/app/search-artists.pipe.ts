83ff9f384f64f084a8dc7e8e91c665d084134eef
xmq291 <xmq291@gmail.com>
2018-09-30 21:10:15 -0400

    learning-angular5
    initial commit

---
+++ src/app/search-artists.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchArtists'
+})
+export class SearchArtistsPipe implements PipeTransform {
+
+  transform(pipeData, pipeModifier) : any {
+    return pipeData.filter(eachItem => {
+      return (
+        eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()) ||
+        eachItem['reknown'].toLowerCase().includes(pipeModifier.toLowerCase()) 
+      )
+    });
+  }
+
+}
